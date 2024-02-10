1fd824a266dc023b25e43eb63bbda722454ac7b5
zwarott <zwarott@email.cz>
2024-02-10 19:05:00 +0100

    Add logging setup for data scraping purposes.

---
+++ src/logging_setup.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def logging_setup():
+    """Set up logging configuration."""
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
