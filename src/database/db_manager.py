ea3e03cfd85f99b9e51ebed54734a78fa2b0d235
zwarott <zwarott@email.cz>
2024-02-20 10:25:14 +0100

    Bulk data scraping and updating db tables.

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -4,8 +4,16 @@ import pandas as pd
 from sqlalchemy import select
 
 from src.session_config import Session
-from src.data_models.base import Base
 from src.database.decorators import timer
+
+from src.data_models.base import Base
+from src.data_models.game import Game
+from src.data_models.team import TeamStat, TeamStatAdvanced
+from src.data_models.player import SkaterStat, SkaterStatAdvanced
+
+from src.data_preparation.game_data import games_last
+from src.data_preparation.team_data import basic_team_stats, advanced_team_stats
+from src.data_preparation.player_data import basic_skater_stats, advanced_skater_stats
 
 
 @timer
@@ -52,4 +60,24 @@ def populate_db_table(class_obj: Type[Base], df: pd.DataFrame) -> None:
               )
 
 
-# delete_data 
+@timer
+def update_all_tables(last_game_date: str) -> None:
+    """Update all database tables.
+
+    Scrape all missing data and insert them into game,
+    team and player tables.
+    """
+    # Append last games stats
+    populate_db_table(Game, games_last(last_game_date))
+
+    # Append last basic team stats
+    populate_db_table(TeamStat, basic_team_stats())
+
+    # Append last advanced team stats
+    populate_db_table(TeamStatAdvanced, advanced_team_stats())
+
+    # Append last basic skater stats
+    populate_db_table(SkaterStat, basic_skater_stats())
+
+    # Append last advanced skater stats 
+    # populate_db_table(SkaterStatAdvanced, advanced_skater_stats())
